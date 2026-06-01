35046886e3a5e8181f327b52660242faa088c8ef
divyanA615-web <divyandon615@gmail.com>
2026-06-01 13:04:38 +0530

    now  i compleated the step 2 (clean the dataset to handle  missing values and duplicate values)of task

--- task1_iris.py
+++ task1_iris.py
@@ -20,9 +20,15 @@ classification_report, # type: ignore
 confusion_matrix, # pyright: ignore[reportUnusedImport] # type: ignore # type: ignore # type: ignore
 ConfusionMatrixDisplay) # type: ignore
 
-# -- 1.load the dataset--
+# -- 1.load the dataset --
 Iris = pd.read_csv(r"D:\data science related\iris_flower_classification\Iris-Flower-Classification\Iris.csv")
 print("=== IRIS DATASET LOADED===")
 print(f"shape: {Iris.shape}")
 print(f"columns: {Iris.columns.to_list()}")
 print(f"\nFirst 5 rows:\n{Iris.head()}")
+
+#-- 2. clean the dataset --
+print(f"\n--- Missing Values ---\n{Iris.isnull().sum()}")
+print(f"\n--- Duplicate Rows ---\n{Iris.duplicated().sum()} ---")
+Iris = Iris.drop_duplicates()
+Iris = Iris.drop(columns=['Id']) #Id is not a feature
